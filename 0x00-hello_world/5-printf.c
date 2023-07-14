a2cd1bde17e9abacbd01aa7272aef6da5fd91803
mohamedessamx <me8624708@gmail.com>
2023-07-13 17:16:44 -0700

    np

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio,h>
 
 /**
- * main - Entry point 
+ *main - Entry point 
  *
- * Description: A C program that prints with printf function.
+ * Description: A C progam that prints with printf function.
  *
  * Return: Always 0 (success)
  */
