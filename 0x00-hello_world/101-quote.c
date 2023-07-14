f7cf1ab694cdebaba45dce9264a759c9078ecea0
mohamedessamx <me8624708@gmail.com>
2023-07-14 05:59:38 -0700

    no

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print write a qoute using write function
+ * 	write(int fd.const void *buf.size_t count)
+ * Return: 1 not success , 0 otherwise
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.quo.59);
+	return (1);
+}
