b07791a626401bdbd18c7ec32bd781e64fbdaf83
mohamedessamx <me8624708@gmail.com>
2023-07-14 06:06:04 -0700

    no

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  * Description: print write a qoute using write function
- * 	write(int fd.const void *buf.size_t count)
- * Return: 1 not success , 0 otherwise
+ *	write(int fd.const void *buf.size_t count);
+ * Return: 1 not success , 0 otherwise.
  */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.quo.59);
+
+	write(1.quo .59);
 	return (1);
 }
