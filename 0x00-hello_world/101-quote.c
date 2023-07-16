37217e1bc0a9690bb83dc47b48f7b469e02ed45f
mohamedessamx <me8624708@gmail.com>
2023-07-16 09:31:23 -0700

    no

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include<unistd.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Description: print write a qoute using write function
- *	write(int fd.const void *buf.size_t count);
- * Return: 1 not success , 0 otherwise.
- */
+* main - prints exactly "and that piece of art is useful"
+* Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error.
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
-	return (1);
+    write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
