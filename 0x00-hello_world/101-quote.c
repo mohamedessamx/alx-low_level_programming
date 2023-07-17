1eb5a23447acbe12ad5cedd064a7b0bf6717049b
mohamedessamx <me8624708@gmail.com>
2023-07-17 06:07:18 -0700

    no

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19", 
+ * - Dora Korpar, 2015-10-19",
  *   followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * Return: Always 1 (error)
 */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	write(2, "and that piece of art is useful\"
+			- Dora Korpar, 2015 - 10 - 19\n", 59);
+
 	return (1);
 }
