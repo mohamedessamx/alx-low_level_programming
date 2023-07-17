e9bb4d1c998f5597c58f29749b576228d09fda73
mohamedessamx <me8624708@gmail.com>
2023-07-17 09:08:54 -0700

    no

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 0, y;
+
+	while (x <= 99)
+	{
+		y = x;
+		while (y <= 99)
+		{
+			if (y != x)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
