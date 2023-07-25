08457900d10841cba80e6b3418a8e9b1b98c0f34
mohamedessamx <me8624708@gmail.com>
2023-07-25 16:30:20 -0700

    no

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	while (*s != '\0')
 	{
 		if (*s == '-')
-			sign *= 1;
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
