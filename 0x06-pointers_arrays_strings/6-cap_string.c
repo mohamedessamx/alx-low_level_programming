9d4347c7429238f8d87561c6c91b08cc99fd8a8f
mohamedessamx <me8624708@gmail.com>
2023-07-27 02:51:21 -0700

    no

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,55 +1,30 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * islower - determine whether ascii is lower
- * @c: character
- * Return: 1 if true, 0 if false
- *
+ * cap_string - capitalizes everey word of a string
+ * @chars: string to modify
+ * Return: the resulting string
  */
-int islower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-/**
- * isDelimiter - detrmine whether ascii is a delimiter
- * @c: character
- * Return: 1 if true, 0 if false
- */
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; i <= 13; i++)
+char cap_string(chars)
+{
+	int i, j;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (c == delimiter[i])
-			return (1);
-	}
-	return (0);
-}
-/**
- * cap_string - function that capitalizes all words of a string.
- * @str: gg
- * Return: string
- */
-char *cap_string(char *str)
-{
-	char *ptr = s;
-	int f = 1;
-
-	while (*s)
-	{
-		if (isDelimiter(*s))
-			f = 1;
-		else if (islower(*s) && f)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		for (j = 0; j < 13; j++)
 		{
-			*s -= 32;
-			f = 0;
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
 		}
-		else
-			f = 0;
-		s++;
 	}
-	return (ptr);
+	return (s);
 }
