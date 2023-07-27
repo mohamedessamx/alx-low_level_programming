1548ab9a9bdb3d70e55d8b15388bb28874afdf7c
mohamedessamx <me8624708@gmail.com>
2023-07-27 02:24:18 -0700

    no

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * _strcmp - compare two string
  * @s1: string 1
  * @s2: string 2
  * Return: 1 if true, 0 if false
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	
+
 	while (*s1)
 	{
 		if (*s1 != *s2)
