710520bcac5be15464604558fc636727b26f152d
mohamedessamx <me8624708@gmail.com>
2023-08-11 16:22:05 -0700

    no

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,75 +1,93 @@
-#include "main.h"
-/**
- * _puts - print string
- * @str: string
- * Return: void
- */
-void _puts(char *str)
-{
-	int i = 0;
+#include <stdio.h>
+#include <stdlib.h>
 
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
+int _isdigit(char *str);
+int _strlen(char *str);
+void _multiply(char *num1, char *num2);
+void _print_result(char *result);
+
+int main(int argc, char *argv[])
+{
+char *num1, *num2;
+
+if (argc != 3)
+{
+	printf("Error\n");
+	return 98;
 }
-/**
- * _atoi - convert string to int
- * @s: string
- * Return: int
- */
-int _atoi(const char *s)
+num1 = argv[1];
+num2 = argv[2];
+if (!_isdigit(num1) || !_isdigit(num2))
 {
-	int sign = 1;
-	unsigned long int resp = 0, firstnum, i;
-
-	for (firstnum = 0; !(s[firstnum] >= 48 && s[firstnum] <= 57); firstnum++)
-	{
-		if (s[firstnum] == '-')
-			sign *= -1;
-	}
-	for (i = firstnum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-	return (sign * resp);
+	printf("Error\n");
+	return 98;
 }
-/**
- * print_int - print int
- * @n: int
- * Return: 0
- */
-void print_int(unsigned long int n)
-{
-	unsigned long int divisor = 1, i, resp;
+_multiply(num1, num2);
+return 0;
+}
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+int _isdigit(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] < '0' || str[i] > '9')
+		return 0;
+}
+return 1;
+}
+
+int _strlen(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
 	;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp
-	}
+return i;
 }
-/**
- * main - multiplies two positive numbers.
- * @argc: int
- * @argv: list
- * Return: 0
- */
-int main(int argc, char const *argv[])
-{
-	(void)argc;
 
-	if (argc != 3)
+void _multiply(char *num1, char *num2)
+{
+int len1, len2, i, j, k;
+int *result;
+int carry, n1, n2, sum;
+
+len1 = _strlen(num1);
+len2 = _strlen(num2);
+result = calloc(len1 + len2, sizeof(int));
+for (i = len1 - 1; i >= 0; i--)
+{
+	n1 = num1[i] - '0';
+	carry = 0;
+	k = len2 + i;
+	for (j = len2 - 1; j >= 0; j--)
 	{
-		_puts("Error");
-		exit(98);
+		n2 = num2[j] - '0';
+		sum = n1 * n2 + result[k] + carry;
+		carry = sum / 10;
+		result[k] = sum % 10;
+		k--;
 	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
-	return (0);
+	if (carry > 0)
+		result[k] += carry;
+}
+_print_result(result);
+free(result);
+}
+
+void _print_result(char *result)
+{
+int i = 0;
+
+while (result[i] == 0)
+	i++;
+if (i == _strlen(result))
+	printf("0\n");
+else
+{
+	for (; i < _strlen(result); i++)
+		printf("%d", result[i]);
+	printf("\n");
+}
 }
