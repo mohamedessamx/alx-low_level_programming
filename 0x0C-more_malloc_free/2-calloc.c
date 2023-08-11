ef569b968df614bf31c529d83df3f2f7e24e35ea
mohamedessamx <me8624708@gmail.com>
2023-08-11 14:27:15 -0700

    no

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _memset - fill memory with constant byte
+ * @s: pointer to put the constant
+ * @c: constant
+ * @n: max byte use
+ * Return: s
+ */
+char *_memset(char *s, char c, int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s-- = b;
+	return (ptr);
+}
+/**
+ * _calloc -  allocates memory for an array, using malloc.
+ * @nmemb: int
+ * @size: int
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	m = malloc(sizeof(int) * nmemb);
+	if (m == 0)
+		return (NULL);
+	_memset(m, 0, sizeof(int) * nmemb);
+	return (m);
+}
