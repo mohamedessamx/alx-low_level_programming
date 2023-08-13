78b70f2424c45554ce134e479f3faa10bf6fee4d
mohamedessamx <me8624708@gmail.com>
2023-08-13 06:30:12 -0700

    no

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,77 +1,84 @@
-#include "main. h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * _puts - prints a string, followed by a new line
- * @str: pointer to the string to print
- * Return: void
+ * multiply - multply two number
+ * @num1: first number
+ * @num2: second number
+ * Return: nothing
  */
-void _puts(char *str)
+void multiply(char *num1, char *num2)
 {
-int i = 0;
+int len1 = 0, len2 = 0, i, j;
+int *result, carry, n1, n2, sum;
 
-while (str[i])
+while (num1[len1])
+	len1++;
+while (num2[len2])
+	len2++;
+result = malloc(sizeof(int) * (len1 + len2));
+if (result == NULL)
 {
-	_putchar (str[il);
+	printf("Error: Memory allocation failed\n");
+	exit(98);
+}
+for (i = 0; i < len1 + len2; i++)
+	result[i] = 0;
+for (i = len1 - 1; i >= 0; i--)
+{
+	carry = 0;
+	n1 = num1[i] - '0';
+	for (j = len2 - 1; j >= 0; j--)
+	{
+		n2 = num2[j] - '0';
+		sum = n1 * n2 + result[i + j + 1] + carry;
+		result[i + j + 1] = sum % 10;
+		carry = sum / 10;
+	}
+	if (carry > 0)
+		result[i] += carry;
+}
+i = 0;
+while (result[i] == 0 && i < len1 + len2 - 1)
 	i++;
-}
-}
-/**
- * _atoi - convert a string to an integer.
- * @s : char type string
- * Return: integer converted
- */
-int _atoi(const char *s)
-{
-int sign = 1;
-unsigned long int resp = 0, firstNum, i;
-
-for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-{
-	if (s[firstNum] == '-')
-	{
-		sign *= -1;
-	}
-}
-for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-{
-	resp *= 10;
-	resp += (s[i] - 48);
-}
-return (sign * resp);
+for (; i < len1 + len2; i++)
+	printf("%d", result[i]);
+printf("\n");
+free(result);
 }
 /**
- * print_int - prints an integer
- * @n: int
- * Return: 0
- */
-void print_int(unsigned long int n)
-{
-	unsigned long int divisor = 1, resp;
-
-	for (i = 0; (n / divisor) > 9; i++, divisor *= 10)
-		;
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
-}
-/**
- * main - print the result of the multiplication, followed by a new line
+ * main - get the two digit
  * @argc: int
  * @argv: list
  * Return: 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-(void) argc;
-
 if (argc != 3)
 {
-	_puts("Error ");
-	exit(98);
+	printf("Error: Incorrect number of arguments\n");
+	return (98);
 }
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
+char *num1 = argv[1];
+char *num2 = argv[2];
+
+/* Check if the numbers contain only digits*/
+for (int i = 0; num1[i]; i++)
+{
+	if (num1[i] < '0' || num1[i] > '9')
+	{
+		printf("Error: Invalid input\n");
+		return (98);
+	}
+}
+for (int i = 0; num2[i]; i++)
+{
+	if (num2[i] < '0' || num2[i] > '9')
+	{
+		printf("Error: Invalid input\n");
+		return (98);
+	}
+}
+/* Multiply the numbers*/
+multiply(num1, num2);
 return (0);
 }
