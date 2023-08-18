86f66e2a047b4eeb9cb7a2cdb7a5383a0a2fbe82
mohamedessamx <me8624708@gmail.com>
2023-08-17 17:00:09 -0700

    no

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,76 +1,85 @@
-#include "variadic_functions.h"
-/**
- * format_char - formats characters
- * @separator: string separator
- * @ap: argument pointer
- */
-void format_char(char *separator, va_list ap)
-{
-	printf("%s%c", separator, va_arg(ap, int));
-}
-/**
- * format_int - formats integers
- * @separator: string separator
- * @ap: argument pointer
- */
-void format_int(char *separator, va_list ap)
-{
-	printf("%s%d", separator, va_arg(ap, int));
-}
-/**
- * format_float - formats float
- * @separator: string separator
- * @ap: argument pointer
- */
-void format_float(char *separator, va_list ap)
-{
-	printf("%s%f", separator, va_arg(ap, int));
-}
-/**
- * format_string - formats string
- * @separator: string separator
- * @ap: argument pointer
- */
-void format_string(char *separator, va_list ap)
-{
-	char *str = va_arg(ap, char*);
-
-	switch ((int)(!str))
-		case 1:
-			str = "(nil)";
-	printf("%s%s", separator, str);
-}
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * print_all -  prints anything.
  * @format: the format string
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
-	char *separator = "";
-	va_list ap;
-	token_t tokens[] = {
-		{"c", format_char},
-		{"i", format_int},
-		{"f", format_float},
-		{"s", format_string},
-		{NULL, NULL}
-	};
-	 va_start(ap, format);
-	while (format && format[i])
+int i = 0;
+char *separator = "";
+va_list ap;
+typedef struct {
+	char format;
+	void (*print_func)(va_list);
+} format_t;
+
+format_t formats[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string},
+	{0, NULL}
+};
+va_start(ap, format);
+while (format && format[i])
+{
+	int j = 0;
+	while (formats[j].format)
 	{
-		j = 0;
-		while (tokens[j].token)
+		if (format[i] == formats[j].format)
 		{
-			if (format[i] == tokens[j].token[0])
-			{
-				tokens[j].f(separator, ap);
-				separator = ", ";
-			}
-			j++;
+			printf("%s", separator);
+			formats[j].print_func(ap);
+			separator = ", ";
+			break;
 		}
-		i++;
+		j++;
 	}
-	printf("\n");
-	va_end(ap);
+	i++;
+}
+printf("\n");
+va_end(ap);
+}
+/**
+ * print_char - formats characters
+ * @ap: argument pointer
+ */
+void print_char(va_list ap)
+{
+char c = va_arg(ap, int);
+
+printf("%c", c);
+}
+/**
+ * print_int - formats integers
+ * @ap: argument pointer
+ */
+void print_int(va_list ap)
+{
+int n = va_arg(ap, int);
+
+printf("%d", n);
+}
+/**
+ * print_float - formats float
+ * @ap: argument pointer
+ */
+void print_float(va_list ap)
+{
+double f = va_arg(ap, double);
+
+printf("%f", f);
+}
+/**
+ * print_string - formats strin
+ * @ap: argument pointer
+ */
+void print_string(va_list ap)
+{
+char *str = va_arg(ap, char*);
+
+if (str == NULL)
+	str = "(nil)";
+printf("%s", str);
 }
