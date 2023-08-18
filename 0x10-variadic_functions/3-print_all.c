9d61584ee07f67ef722f6adc7da470bdf23c59a2
mohamedessamx <me8624708@gmail.com>
2023-08-18 05:20:05 -0700

    no

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,85 +1,48 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
+
 /**
- * print_all -  prints anything.
- * @format: the format string
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-int i = 0;
-char *separator = "";
-va_list ap;
-typedef struct {
-	char format;
-	void (*print_func)(va_list);
-} format_t;
+	int i = 0;
+	char *str, *sep = "";
 
-format_t formats[] = {
-	{'c', print_char},
-	{'i', print_int},
-	{'f', print_float},
-	{'s', print_string},
-	{0, NULL}
-};
-va_start(ap, format);
-while (format && format[i])
-{
-	int j = 0;
-	while (formats[j].format)
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		if (format[i] == formats[j].format)
+		while (format[i])
 		{
-			printf("%s", separator);
-			formats[j].print_func(ap);
-			separator = ", ";
-			break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		j++;
 	}
-	i++;
-}
-printf("\n");
-va_end(ap);
-}
-/**
- * print_char - formats characters
- * @ap: argument pointer
- */
-void print_char(va_list ap)
-{
-char c = va_arg(ap, int);
 
-printf("%c", c);
-}
-/**
- * print_int - formats integers
- * @ap: argument pointer
- */
-void print_int(va_list ap)
-{
-int n = va_arg(ap, int);
-
-printf("%d", n);
-}
-/**
- * print_float - formats float
- * @ap: argument pointer
- */
-void print_float(va_list ap)
-{
-double f = va_arg(ap, double);
-
-printf("%f", f);
-}
-/**
- * print_string - formats strin
- * @ap: argument pointer
- */
-void print_string(va_list ap)
-{
-char *str = va_arg(ap, char*);
-
-if (str == NULL)
-	str = "(nil)";
-printf("%s", str);
+	printf("\n");
+	va_end(list);
 }
