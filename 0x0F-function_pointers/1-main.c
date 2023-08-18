e5578866a07d21bf8348ecb372e759ec931c0e0b
mohamedessamx <me8624708@gmail.com>
2023-08-18 13:07:27 -0700

    no

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator -  executes a function given as a parameter
- *			on each element of an array
+ *                      on each element of an array
  * @array: int array
  * @size: array size
  * @action: function pointer
@@ -9,9 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array == NULL || action == NULL)
-		return;
+        if (array == NULL || action == NULL)
+                return;
 
-	for (size_t i = 0; i < size; i++)
-			action(array[i]);
+        for (size_t i = 0; i < size; i++)
+                        action(array[i]);
 }
