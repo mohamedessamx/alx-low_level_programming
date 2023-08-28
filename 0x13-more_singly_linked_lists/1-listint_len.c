0610735338d5c51c455705848ef1498c3f446b63
mohamedessamx <me8624708@gmail.com>
2023-08-28 05:50:21 -0700

    no

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to first node
+ * Return: number of element in list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to first node
+ * @n: int to add
+ * Return: address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+
+	if (!head || !newnode)
+		return (NULL);
+	newnode->next = NULL;
+	newnode->n = n;
+	if (*head)
+		newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
