569e403dd5e34c81395c3eca4a4df1b58a928f0f
mohamedessamx <me8624708@gmail.com>
2023-08-30 10:29:07 -0700

    no

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * find_listint_loop_fl - finds a loop in alinked list
+ * find_listint_loop - finds a loop in alinked list
  * @head: linked list to search
  * Return: address of node where loop sarat/returns, NULL if no loop
  */
-listint_t *find_listint_loop_fl(listint_t *head)
+listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *ptr, *end;
 
