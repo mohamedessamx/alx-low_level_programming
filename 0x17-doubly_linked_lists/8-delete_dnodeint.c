facdc07a14f2b00144c390b4dbb071a4b45dca78
mohamedessamx <me8624708@gmail.com>
2023-10-13 11:19:50 -0700

    l

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 
-	if (*head ==NULL)
+	if (*head == NULL)
 		return (-1);
 	for (; index != 0; index--)
 	{
@@ -25,7 +25,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp->orev->next = tmp->next;
+		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
